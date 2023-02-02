bcf09ff58a39c72d42fbb66be43145bbb7a46a61
Dev-R <rabimhd@icloud.com>
2023-02-03 00:54:05 +0800

    Remove unused code

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,14 +1,7 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <RouterView />
+  <RouterView/>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
