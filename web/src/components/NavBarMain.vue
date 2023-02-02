62ef8d7a3967b134b8dadf9a0707b6d0ae8baa58
Dev-R <rabimhd@icloud.com>
2023-02-03 01:03:02 +0800

    Add Navbar draft

---
+++ web/src/components/NavBarMain.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full">
+        <p>This is NavBarMain</p>
+    </div>
+</template>
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'NavBarMain',
+    setup() {
+        onMounted(() => {
+            // console.log('Mounted NavBarMain')
+        })
+
+        return {}
+    }
+})
+</script>
