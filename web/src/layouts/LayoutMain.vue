7fb3f39dc699b899ecadf7a9e4164eac11f4a1f3
Dev-R <rabimhd@icloud.com>
2023-02-03 00:54:27 +0800

    Add main layout

---
+++ web/src/layouts/LayoutMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <div
+        class="min-h-screen bg-white">
+        <RouterView/>
+    </div>
+</template>
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'LayoutMain',
+    setup() {
+        onMounted(() => {
+            // console.log('Mounted LayoutMain')
+        })
+
+        return {}
+    }
+})
+</script>
