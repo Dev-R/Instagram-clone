f11d27f1d55d7da83eeb97dc2011f9087b2afc84
Dev-R <rabimhd@icloud.com>
2023-02-14 02:57:48 +0800

    Draft HomeView grid

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,6 +1,18 @@
 <template>
-    <div class="w-full">
-        <p>This is Home</p>
+    <div class="bg-gray-400">
+        <section class="container max-w-full mx-auto text-center">
+            <div class="grid grid-cols-12 gap-1">
+                <div class="col-span-2 p-4 rounded-lg shadow-lg bg-fuchsia-500 md:block hidden">
+                    A
+                </div>
+                <div class="lg:col-span-8 md:col-span-10 col-span-12 p-4 rounded-lg shadow-lg bg-fuchsia-500 h-screen">
+                    B
+                </div>
+                <div class="col-span-2 p-4 rounded-lg shadow-lg bg-fuchsia-500 lg:block hidden">
+                    C
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -18,3 +30,10 @@ export default defineComponent({
     }
 })
 </script>
+
+<!-- 
+   A : 2 
+   B : 8
+   C : 2
+
+-->
