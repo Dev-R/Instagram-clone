d0b62378c3a41ce3f7b38b4a536ccf3ff85d0bd9
Dev-R <rabimhd@icloud.com>
2023-02-18 02:20:25 +0800

    Draft HomeView grid

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,16 +1,211 @@
 <template>
-    <div class="bg-gray-400">
+    <div class="bg-gray-900">
         <section class="container max-w-full mx-auto text-center">
             <div class="grid grid-cols-12 gap-1">
-                <div class="col-span-2 p-4 rounded-lg shadow-lg bg-fuchsia-500 md:block hidden">
-                    A
+                <!-- Left bar: Navigation -->
+                <div class="col-span-2 p-4 rounded-lg shadow-lg bg-black md:block hidden space-y-12">
+                    <!-- A -->
+                    <div class="flex flex-col space-y-4 flex-nowrap sticky top-0">
+                        <!-- A -->
+                        <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg">
+                            <img src="http://via.placeholder.com/287x29">
+                        </div>
+                        <!-- B -->
+                        <div class="p-4 rounded-lg flex justify-start flex-col justify-start justify-center bg-gray-500 shadow-lg  space-y-4">
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>1</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>2</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>3</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>4</span>
+                            </div>
+
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>5</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>6</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>7</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>8</span>
+                            </div>
+                        </div>
+                        <!-- C -->
+                        <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg">
+                            X
+                        </div>
+                    </div>
+                    
                 </div>
-                <div class="lg:col-span-8 md:col-span-10 col-span-12 p-4 rounded-lg shadow-lg bg-fuchsia-500 h-screen">
-                    B
+
+                <!-- Center: Posts -->
+                <div class="lg:flex lg:col-span-6 md:col-span-10 col-span-12 p-4 rounded-lg shadow-lg bg-black">
+                    <div class="lg:w-96 lg:invisible">
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div class="mx-auto  flex flex-col space-y-4 flex-nowrap lg:w-[28rem]">
+                        <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg">
+                            <img src="http://via.placeholder.com/287x29">
+                        </div>
+                        <div class="p-4 rounded-lg flex justify-start flex-col justify-start justify-center bg-gray-500 shadow-lg  space-y-4">
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>1</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>2</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>3</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>4</span>
+                            </div>
+
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>5</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>6</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>7</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>8</span>
+                            </div>
+
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>1</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>2</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>3</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>4</span>
+                            </div>
+
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>5</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>6</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>7</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>8</span>
+                            </div>
+
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>1</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>2</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>3</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>4</span>
+                            </div>
+
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>5</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>6</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>7</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>8</span>
+                            </div>
+                            
+                        </div>
+                    </div>
                 </div>
-                <div class="col-span-2 p-4 rounded-lg shadow-lg bg-fuchsia-500 lg:block hidden">
-                    C
+
+                <!-- Right bar: Suggestions -->
+                <div class="col-span-4 p-4 rounded-lg shadow-lg bg-black lg:block hidden">
+                    <div class="flex flex-col space-y-4 flex-nowrap top-0 w-80">
+                        <!-- A -->
+                        <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg">
+                            <img src="http://via.placeholder.com/287x29">
+                        </div>
+                        <!-- B -->
+                        <div class="p-4 rounded-lg flex justify-start flex-col justify-start justify-center bg-gray-500 shadow-lg  space-y-4">
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>1</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>2</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>3</span>
+                            </div>
+                            <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg space-x-2">
+                                <img src="http://via.placeholder.com/26x26">
+                                <span>4</span>
+                            </div>
+                        </div>
+                        <!-- C -->
+                        <div class="p-4 rounded-lg flex items-center justify-center bg-indigo-500 shadow-lg">
+                            X
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </section>
     </div>
