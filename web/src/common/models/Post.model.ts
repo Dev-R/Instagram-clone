03e6b410ec571ce869b185ef62fde3d9e9efde76
Dev-R <rabimhd@icloud.com>
2023-02-28 03:46:31 +0800

    Add StoryCarousel, SuggestionCard to model

--- web/src/common/models/Post.model.ts
+++ web/src/common/models/Post.model.ts
@@ -1,4 +1,7 @@
-/** Interface for a single media in a post */
+
+/** 
+ * Interface for a single media in a post 
+ */
 export interface PostMedia {
     /** Media index in the carousel */
     index: number
@@ -13,3 +16,84 @@ export interface PostMedia {
     /** Media Title/name */
     title?: string
   }
+
+
+/**
+ * Interface for a single post card
+ */
+export interface PostCard {
+    /** Array of media in post carousel */
+    carouselMedia: PostMedia[]
+  
+    /** Number of likes for the post */
+    likeCount: number
+  
+    /** Whether the current user has liked the post */
+    hasLiked: boolean
+  
+    /** Caption for the post */
+    caption: string
+  
+    /** Username of the poster */
+    userName: string
+  
+    /** URL of the poster's profile picture */
+    profilePictureUrl: string
+
+    /** Unix timestamp representing when the post was created */
+    createdAt: string | number
+  
+    /** Total number of comments on the post */
+    commentCount: number
+  }
+  
+/**
+ * Interface for Story Carousel model
+ */
+export interface StoryCarousel {
+    // /** The user who created the story carousel */
+    // user: User,
+
+    /** User's user name */
+    userName: string,
+  
+    /** URL of the user's profile picture */
+    profilePictureUrl: string,
+
+    /** The time when the story carousel expires */
+    expiringAt: string,
+    
+    /** Whether the user has seen the story carousel or not */
+    seen: boolean,
+    
+    /** The list of media items in the story carousel */
+    items: PostMedia[],
+    
+    /** The total number of media items in the story carouse */
+    mediaCount: number
+}
+
+
+/**
+ * Interface for a suggestion card.
+ */
+export interface SuggestionCard {
+
+    /** Username of the user being suggested. */
+    userName: string,
+
+    /** URL of the user's profile picture being suggested. */
+    profilePictureUrl: string,
+
+    /** Information about the user that is being suggested to follow. */
+    suggested: {
+        /** Username of the user that is being suggested to follow. */
+        userName: string,
+
+        /** URL of the user's profile picture that is being suggested to follow. */
+        profilePictureUrl: string,
+
+        /** Username of the user that follows the suggested user. */
+        followedBy: string,
+    }[]
+}
