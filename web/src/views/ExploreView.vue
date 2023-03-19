3d9e84043b64f432aa65787bd07a076c100fc9d3
Dev-R <rabimhd@icloud.com>
2023-03-20 03:41:01 +0800

    Draft Explore

--- web/src/views/ExploreView.vue
+++ web/src/views/ExploreView.vue
@@ -1,20 +1,76 @@
 <template>
-    <div class="w-full">
-        <p>This is Explore</p>
+    <div class="container w-[470px] p-2">
+        <PostCard 
+            :post-item="postItem"/>
     </div>
 </template>
 
 <script lang="ts">
 import { onMounted, defineComponent } from 'vue'
+import PostCard from '@/components/basics/PostCard.vue';
+import type { PostMedia } from '@/common/models/Post.model';
+
 
 export default defineComponent({
     name: 'Explore',
     setup() {
+
+        const mediasArraySampleA: PostMedia[] = [
+            {
+                index: 0,
+                type: 'image',
+                mediaUrl:
+                    "https://loremflickr.com/1024/1280/cat",
+                title: "Legendary A"
+            },
+            {
+                index: 1,
+                type: 'image',
+                mediaUrl:
+                    "https://loremflickr.com/1024/1280/nature",
+                title: "Legendary A"
+            }
+        ]
+
+        const mediasArraySampleB: PostMedia[] = [
+            {
+                index: 0,
+                type: 'image',
+                mediaUrl:
+                    "https://loremflickr.com/1024/1280/sky",
+                title: "Legendary A"
+            },
+            {
+                index: 1,
+                type: 'image',
+                mediaUrl:
+                    "https://loremflickr.com/1024/1280/love",
+                title: "Legendary A"
+            }
+        ]
+
+        const postItem = {
+            id: '0',
+            userName: 'Rabee',
+            createdAt: '',
+            likeCount: 0,
+            hasLiked: true,
+            caption: ' Sh. @abdullah_oduro and I getting that Saturday morning work in the gym and talking over @yaqeeninstitute Quran 30 ',
+            carouselMedia: mediasArraySampleA,
+            commentCount: 0,
+            profilePictureUrl: 'https://loremflickr.com/32/32/bird'
+        }
+
         onMounted(() => {
             // console.log('Mounted Explore')
         })
 
-        return {}
+        return {
+            postItem
+        }
+    },
+    components: {
+        PostCard
     }
 })
 </script>
