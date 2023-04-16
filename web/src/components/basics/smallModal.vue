3158075a0c2029f69c3b2b844f7e3f3a3cf3cadc
Dev-R <rabimhd@icloud.com>
2023-04-17 02:18:39 +0800

    Updated import casing

--- web/src/components/basics/smallModal.vue
+++ web/src/components/basics/smallModal.vue
@@ -61,7 +61,7 @@
 import { defineComponent, onMounted } from 'vue'
 
 import type { SuggestionCard } from '@/common/models/post.model'
-import SmallCard from './smallCard.vue'
+import SmallCard from './SmallCard.vue'
 import SVGLoader from '@/components/basics/SVGLoader.vue'
 
 export default defineComponent({
