16231a6ec9a73fec8d1b51d28b56fdf95069ce0b
Dev-R <rabimhd@icloud.com>
2023-04-18 02:02:24 +0800

    Add event emitting handlers for mobile navbar

--- web/src/layouts/LayoutMain.vue
+++ web/src/layouts/LayoutMain.vue
@@ -1,15 +1,17 @@
 <template>
     <div
         class="min-h-screen min-w-screen bg-black">
-        <TopNavBar />
-        <RouterView />
+        <TopNavBar 
+            @on-create="triggerMobileFileUpload"/>
+        <RouterView 
+            :call-mobile-file-upload-trigger="isTriggered "/>
         <!-- Mobile Navbar -->
         <NavBarMobile />
     </div>
 </template>
 
 <script lang="ts">
-import { onMounted, defineComponent } from 'vue';
+import { onMounted, defineComponent, ref } from 'vue';
 
 import NavBarMobile from '@/components/navbars/BottomNavBar.vue';
 import SVGLoader from '@/components/basics/SVGLoader.vue';
@@ -20,11 +22,26 @@ import TopNavBar from '@/components/navbars/TopNavBar.vue';
 export default defineComponent({
     name: 'LayoutMain',
     setup() {
+
+        // Checkers
+        const isTriggered  = ref<boolean>(false)
+
+        // Methods
+        /**
+         * Trigger fileUpload in Home route using mobile Navbar
+         */
+        const triggerMobileFileUpload = () => {
+            isTriggered.value = !isTriggered.value
+        }
+
         onMounted(() => {
             // console.log('Mounted LayoutMain')
         })
 
-        return {}
+        return {
+            triggerMobileFileUpload,
+            isTriggered
+        }
     },
     components: {
     NavBarMobile,
