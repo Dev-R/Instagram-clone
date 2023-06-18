9a2e37fe68aac40906372e8b2552da9423d6f3aa
Dev-R <rabimhd@icloud.com>
2023-06-19 02:25:04 +0800

    Update component imports

--- web/src/components/cards/index.ts
+++ web/src/components/cards/index.ts
@@ -1,9 +1,11 @@
 import PostCard from "./PostCard.vue"
 import SmallCard from "./SmallCard.vue"
 import SuggestionCard from "./SuggestionCard.vue"
+import ReelCard from "./ReelCard.vue"
 
 export {
     PostCard,
     SmallCard,
+    ReelCard,
     SuggestionCard
 }
