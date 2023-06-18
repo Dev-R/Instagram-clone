51c0fc168d1583cb47695847766b3207478f5786
Dev-R <rabimhd@icloud.com>
2023-06-19 02:24:46 +0800

    Add ReelMedia interface

--- web/src/common/models/reel.model.ts
+++ web/src/common/models/reel.model.ts
@@ -1,9 +1,33 @@
-import type { PostCard } from "./post.model"
-import type { BaseUser } from "./user.model"
+import type { BaseCard } from "./post.model"
+
+
+/** 
+ * Interface for a single media in a reel 
+ */
+export interface ReelMedia {
+    /** Media type */
+    type: 'video'
+
+    /** Media URL */
+    mediaUrl: string
+
+    /** Media width in pixels */
+    width?: number
+
+    /** Media height in pixels */
+    height?: number
+
+    /** Media Title/name */
+    title?: string
+
+    /** Media location */
+    location?: string
+}
+
 
 /**
  * Represents a single reel post
  */
-export interface ReelPost extends PostCard{
-    
+export interface ReelPost extends BaseCard{
+    reelMedia : ReelMedia
 }
