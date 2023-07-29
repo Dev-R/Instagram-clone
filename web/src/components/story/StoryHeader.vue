8f56fd51203b9f86e113c580e82f99718b1a5a3c
Dev-R <Rabimhd@icloud.com>
2023-07-29 23:00:53 +0300

    Improve StoryHeader

--- web/src/components/story/StoryHeader.vue
+++ web/src/components/story/StoryHeader.vue
@@ -1,189 +1,201 @@
 <template>
-    <div class="absolute py-2 p-3 text-lg top-5 flex flex-row flex-nowrap justify-between w-11/12">
+	<div class="absolute py-2 p-3 text-lg top-5 flex flex-row flex-nowrap justify-between w-11/12">
+		<!-- User Info -->
+		<div class="flex flex-row space-x-2">
+			<!-- Picture -->
+			<img 
+				class="w-8 h-8 rounded-full object-cover relative"
+				:src="story.profilePictureUrl" />
 
-        <!-- User Info -->
-        <div class="flex flex-row space-x-2">
-            <!-- Picture -->
-            <img 
-                class="w-8 h-8 rounded-full object-cover relative"
-                :src="story.profilePictureUrl"/>
-
-            <!-- Username -->
-            <div class="text-sm text-white self-center space-x-1">
-                <span>
-                    {{ story.userName }}
-                </span>
+			<!-- Username -->
+			<div class="text-sm text-white self-center space-x-1">
+				<span>
+					{{ story.userName }}
+				</span>
                 
-                <span>
-                    {{ story.expiringAt }}
-                </span>
-            </div>
-        </div>
+				<span>
+					{{ story.expiringAt }}
+				</span>
+			</div>
+		</div>
 
-        <!-- Story Controllers -->
-        <div class="flex flex-row space-x-4 pt-2 text-white self-end">
-            <!-- Players -->
-            <span @click="onStoryPlay">
-                <i 
-                    v-if="isStoryPlaying"
-                    class="fa-solid fa-pause
+		<!-- Story Controllers -->
+		<div class="flex flex-row space-x-4 pt-2 text-white self-end">
+			<!-- Players -->
+			<span @click="onStoryPlay">
+				<i 
+					v-if="isStoryPlaying"
+					class="fa-solid fa-pause
                     cursor-pointer"></i>
-                <i 
-                    v-else
-                    class="fa-solid fa-play 
+				<i 
+					v-else
+					class="fa-solid fa-play 
                     cursor-pointer"></i>
-            </span>
+			</span>
 
-            <!-- Muters -->
-            <span @click="onStoryMute">
-                <i 
-                    v-if="isStoryMuted"
-                    class="fa-solid fa-volume-xmark
+			<!-- Muters -->
+			<span @click="onStoryMute">
+				<i 
+					v-if="isStoryMuted"
+					class="fa-solid fa-volume-xmark
                     Mutecursor-pointer"></i>
-                <i 
-                    v-else
-                    class="fa-solid fa-volume-high
+				<i 
+					v-else
+					class="fa-solid fa-volume-high
                     cursor-pointer"></i>
-            </span>
+			</span>
 
-            <!-- Others -->
-            <i class="fa-solid fa-ellipsis self-center cursor-pointer hidden sm:block"></i>
-            <i 
-                @click="onModalClosed()"
-                class="fa-solid fa-x self-center cursor-pointer block sm:hidden"></i>
-        </div>
-
-    </div>
-
+			<!-- Others -->
+			<i class="fa-solid fa-ellipsis self-center cursor-pointer hidden sm:block"></i>
+			<i 
+				class="fa-solid fa-x self-center cursor-pointer block sm:hidden"
+				@click="onModalClosed()"></i>
+		</div>
+	</div>
 </template>
 
 
-<script lang="ts">
-import { defineComponent, ref, watchEffect, type PropType } from 'vue'
+<script setup lang="ts">
+import {  
+	watchEffect, 
+	ref,
+	type PropType 
+} from 'vue'
 
-import { useToast } from 'vue-toastification'
+import { 
+	useToast 
+} from 'vue-toastification'
 
 import type {
-    StoryCarousel
+	StoryCarousel,
+	StoryMedia,
+	StoryType
 } from '@/common'
 
-export default defineComponent({
-    name: 'StoryHeader',
-    setup(prop, context) {
+const props = defineProps({
+	story: {
+		type: Object as PropType<StoryCarousel>,
+		required: true
+	},
+	activeStoryMedia: {
+		type: null as unknown as PropType<StoryMedia>,
+		required: true
+	},
+	activeStoryType: {
+		type: null as unknown as PropType<StoryType>,
+		required: true
+	},
+	isCommentInputFocused: {
+		type: Boolean,
+		required: true
+	}
+})
 
-        const isStoryPlaying = ref(true) // All stories are playing by default
-        const isStoryMuted = ref(true)
+const emit = defineEmits([
+	'onModalClosed',
+	'onResumeStory',
+	'onPauseStory'
+])
 
-        // Services
-        const toast = useToast()
+const isStoryPlaying = ref(true) // All stories are playing by default
+const isStoryMuted = ref(true)
 
-        // Video controllers
-        /**
-         * Play story video if the current story contains a video
-         */
-         const playStoryVideo = () => {
-            if (prop.activeStoryType === 'Video') {
-                const video = prop.activeStory as HTMLVideoElement
-                video?.play()
-                matchStoryVideoMute(video)
-            }
-        }
-
-        /**
-         * Pause story video if the current story contains a video
-         */
-        const pauseStoryVideo = () => {
-            if (prop.activeStoryType === 'Video') {
-                const video = prop.activeStory as HTMLVideoElement
-                video?.pause()
-            }
-        }
-
-        /**
-         * Mute story video if the current story contains a video
-         */
-        const invertMuteStoryVideo = () => {
-            if (prop.activeStoryType === 'Video') {
-                const video = prop.activeStory as HTMLVideoElement
-                video.muted = !video.muted
-                matchStoryVideoMute(video)
-            } else {
-                unsupportedMediaMute()
-            }
-        }
-
-        const matchStoryVideoMute = (video: HTMLVideoElement) => {
-            isStoryMuted.value = video.muted
-        }
-
-        const muteStory = () => {
-            isStoryMuted.value = true
-        }
-
-        const unmuteStory = () => {
-            isStoryMuted.value = false
-        }
+// Services
+const toast = useToast()
 
 
-        const unsupportedMediaMute = () => {
-            toast.info('Video has no sound.')
-        }
+// Video controllers
+/**
+ * Play story video if the current story contains a video
+ */
+const playStoryVideo = () => {
+	if (props.activeStoryType === 'Video') {
+		const video = props.activeStoryMedia as HTMLVideoElement
+		video?.play()
+		matchStoryVideoMute(video)
+	}
+}
+
+/**
+ * Pause story video if the current story contains a video
+ */
+const pauseStoryVideo = () => {
+	if (props.activeStoryType === 'Video') {
+		const video = props.activeStoryMedia as HTMLVideoElement
+		video?.pause()
+	}
+}
+
+/**
+ * Mute story video if the current story contains a video
+ */
+const invertMuteStoryVideo = () => {
+	if (props.activeStoryType === 'Video') {
+		const video = props.activeStoryMedia as HTMLVideoElement
+		video.muted = !video.muted
+		matchStoryVideoMute(video)
+	} else {
+		unsupportedMediaMute()
+	}
+}
+
+const matchStoryVideoMute = (video: HTMLVideoElement) => {
+	isStoryMuted.value = video.muted
+}
+
+const muteStory = () => {
+	isStoryMuted.value = true
+}
+
+// const unmuteStory = () => {
+//     isStoryMuted.value = false
+// }
 
 
-        const onStoryPlay = () => {
-            isStoryPlaying.value = !isStoryPlaying.value
-        }
+const unsupportedMediaMute = () => {
+	toast.info('Video has no sound.')
+}
 
-        const onStoryMute = () => {
-            invertMuteStoryVideo()
-        }
 
-        /**
-        * Emit signal when the modal is closed
-        * @event modal-closed
-        */
-        const onModalClosed = () => {
-            context.emit('onModalClosed')
-        }
+const onStoryPlay = () => {
+	isStoryPlaying.value = !isStoryPlaying.value
+}
 
-        /**
-         * Handle story pauses and plays
-         */
-         watchEffect(() => {
-            isStoryPlaying.value ? context.emit("onResumeStory") : context.emit("onPauseStory")
-        })
+const playStory = () => {
+	isStoryPlaying.value = true
+}
 
-        
-        watchEffect(() => {
-            prop.activeStoryType === 'Video' ? playStoryVideo() : muteStory()
-        })
+const pauseStory = () => {
+	isStoryPlaying.value = false
+}
 
-        return {
-            isStoryPlaying,
-            isStoryMuted,
-            onStoryPlay,
-            onStoryMute,
-            onModalClosed
-        }
-    },
-    props: {
-        story: {
-        type: Object as PropType<StoryCarousel>,
-        required: true
-        },
-        activeStory: {
-            type: null as unknown as PropType<HTMLVideoElement | HTMLImageElement | undefined>,
-            required: true
-        },
-        activeStoryType: {
-            type: Object as PropType<'Image' | 'Video' | null>,
-            required: true
-        },
-    },
-    emits: [
-		'onModalClosed',
-        'onResumeStory',
-        'onPauseStory'
-	],
+const onStoryMute = () => {
+	invertMuteStoryVideo()
+}
+
+/**
+* Emit signal when the modal is closed
+* @event modal-closed
+*/
+const onModalClosed = () => {
+	emit('onModalClosed')
+}
+
+/**
+ * Handle story pauses and plays
+ */
+watchEffect(() => {
+	isStoryPlaying.value ? emit('onResumeStory') : emit('onPauseStory')
+})
+
+watchEffect(() => {
+	props.activeStoryType === 'Video' ? playStoryVideo() : muteStory()
+})
+
+defineExpose({
+	playStoryVideo,
+	pauseStoryVideo,
+	pauseStory,
+	playStory
 })
 </script>
