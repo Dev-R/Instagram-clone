290f2084eb48c6f36230b93d42c8fdb067188330
Dev-R <Rabimhd@icloud.com>
2023-08-15 01:58:43 +0300

    Add SettingView draft

---
+++ web/src/views/SettingView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="bg-slate-1000">
+        <div class="text-lg">This is SettingView</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { onMounted, defineComponent } from 'vue'
+import NavBarMain from '@/components/navbars/NavBarMain.vue'
+
+export default defineComponent({
+    name: 'SettingView  ',
+    setup() {
+        onMounted(() => {
+            // console.log('SettingView mounted')
+        })
+
+        return {}
+    },
+    components: {
+        NavBarMain,
+    },
+})
+</script>
