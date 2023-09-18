9dcce4a646e56bb4fcd71c4fb13811fdfd083665
Dev-R <Rabimhd@icloud.com>
2023-09-18 19:34:11 +0300

    Add gender support for smallModal

--- web/src/components/modals/SmallModal.vue
+++ web/src/components/modals/SmallModal.vue
@@ -1,18 +1,16 @@
 <template>
-	<div 
-		class="bg-black rounded-lg shadow w-full md:pt-0 pt-5 z-50 
+	<div class="bg-black rounded-lg shadow w-full md:pt-0 pt-5 z-50 
         md:5/6 md:bg-slate-1100 absolute 
         top-1/3 left-1/2 transform -translate-x-1/2 
-        -translate-y-1/2"
-		:class="{
-			'hidden': !isToggled,
-			'max-w-xs': modalSize === ModalSize.SuperSmall,
-			'max-w-sm': modalSize === ModalSize.ExtraSmall,
-			'max-w-md': modalSize === ModalSize.Small,
-			'max-w-lg': modalSize === ModalSize.Medium,
-			'max-w-4xl': modalSize === ModalSize.Large,
-			'max-w-7xl': modalSize === ModalSize.ExtraLarge
-		}">
+        -translate-y-1/2" :class="{
+        	'hidden': !isToggled,
+        	'max-w-xs': modalSize === ModalSize.SuperSmall,
+        	'max-w-sm': modalSize === ModalSize.ExtraSmall,
+        	'max-w-md': modalSize === ModalSize.Small,
+        	'max-w-lg': modalSize === ModalSize.Medium,
+        	'max-w-4xl': modalSize === ModalSize.Large,
+        	'max-w-7xl': modalSize === ModalSize.ExtraLarge
+        }">
 		<!-- Modal header -->
 		<div class="flex p-2 border-b border-gray-600 rounded-t justify-between">
 			<div></div>
@@ -21,27 +19,21 @@
 				{{ title }}
 			</h3>
 
-			<button
-				class="text-gray-400 bg-transparent sm:hover:bg-gray-200 sm:hover:text-gray-900
+			<button class="text-gray-400 bg-transparent sm:hover:bg-gray-200 sm:hover:text-gray-900
                     rounded-lg text-sm p-1 inline-flex items-center dark:sm:hover:bg-gray-600 
-                    dark:sm:hover:text-white"
-				@click="onModalClosed()">
+                    dark:sm:hover:text-white" @click="onModalClosed()">
 				<SVGLoader :icon="'cross'" />
 			</button>
 		</div>
 		<!-- Modal body -->
 		<div class="space-y-6">
-			<div 
-				class="flex flex-col space-y-5 lg:max-h-80 
+			<div class="flex flex-col space-y-5 lg:max-h-80 
                 max-h-full overflow-y-scroll scrollbar">
+
 				<!-- Suggestion Card Modal -->
-				<div 
-					v-for="item of <SuggestionCard[]> items"
-					v-if="modalType === ModalType.Follow"
+				<div v-for="item of <SuggestionCard[]>items" v-if="modalType === ModalType.Follow"
 					class="rounded-lg flex justify-between space-x-2 py-2 px-4">
-					<FollowCard 
-						:show-button="true"
-						:profile-image="item.profilePictureUrl">
+					<FollowCard :show-button="true" :profile-image="item.profilePictureUrl">
 						<template #user-name>
 							{{ item.userName }}
 						</template>
@@ -57,23 +49,37 @@
 				</div>
 
 				<!-- Comment Modal -->
-				<div 
-					v-else-if="modalType === ModalType.Comment"
-					class="p-5">
-					<div
-						v-if="shouldDisplayNoComments"
-						class="flex flex-col place-self-center space-y-2">
-						<span
-							class="font-sans text-2xl text-white font-bold mx-auto">
+				<div v-else-if="modalType === ModalType.Comment" class="p-5">
+					<div v-if="shouldDisplayNoComments" class="flex flex-col place-self-center space-y-2">
+						<span class="font-sans text-2xl text-white font-bold mx-auto">
 							No comments yet.
 						</span>
 					</div>
 
-					<CommentCard 
-						v-for="item of <PostCommentCard[]> items"
-						v-else
-						:comment="item" />
+					<CommentCard v-for="item of <PostCommentCard[]>items" v-else :comment="item" />
 				</div>
+
+				<div 
+					v-else-if="modalType === ModalType.Gender"
+					class="flex flex-col p-5">
+
+					<div 
+						v-for="item of <Gender[]>items"
+						:key="item.name"
+						class="flex items-center mb-4">
+						<input 
+							@click="$emit('onGenderSelected', item.name)"
+							type="radio"
+							class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 
+							dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+						<label 
+							class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 capitalize">
+							{{ item.name }}
+						</label>
+					</div>
+
+				</div>
+
 			</div>
 		</div>
 	</div>
@@ -84,7 +90,8 @@ import { defineComponent, onMounted, computed } from 'vue'
 
 import type {
 	SuggestionCard,
-	PostCommentCard
+	PostCommentCard,
+	Gender
 } from '@/common'
 
 import {
@@ -107,9 +114,9 @@ export default defineComponent({
 		})
 
 		/**
-         * Emit signal when the modal is closed
-         * @event modal-closed
-         */
+		 * Emit signal when the modal is closed
+		 * @event modal-closed
+		 */
 		const onModalClosed = () => {
 			context.emit('onModalClosed')
 		}
@@ -134,7 +141,7 @@ export default defineComponent({
 			required: true
 		},
 		items: {
-			type: Object as () => SuggestionCard[] | PostCommentCard[] | undefined,
+			type: Object as () => SuggestionCard[] | PostCommentCard[] | Gender[] | undefined,
 			required: false
 		},
 		isToggled: {
@@ -151,7 +158,8 @@ export default defineComponent({
 		}
 	},
 	emits: [
-		'onModalClosed'
+		'onModalClosed',
+		'onGenderSelected'
 	],
 })
 </script>
