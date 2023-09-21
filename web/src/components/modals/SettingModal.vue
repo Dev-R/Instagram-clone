32e232efedf9aa556d9bb34cc74daa353f8f21c1
Dev-R <Rabimhd@icloud.com>
2023-09-21 20:55:38 +0300

    Refactor SettingModal

--- web/src/components/modals/SettingModal.vue
+++ web/src/components/modals/SettingModal.vue
@@ -2,85 +2,32 @@
     <div 
         class="bg-black rounded-lg shadow h-5/6 w-full md:pt-0 md:z-0 pt-5 z-50 
         lg:h-auto md:w-[400px] md:bg-slate-1100 absolute 
-        md:top-1/3 top-1/2 left-1/2 transform -translate-x-1/2 
+          top-1/2 left-1/2 transform -translate-x-1/2 
         -translate-y-1/2"
         :class="{ 'hidden': !isToggled }">
         <!-- Modal body -->
-        <div class="space-y-6 rounded-full pb-2">
-                <div 
-                    class="flex flex-col  max-h-full overflow-y-scroll scrollbar">
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Change Password
-                        </span>
-                    </div>
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            QR Code
-                        </span>
-                    </div>
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Apps and Websites
-                        </span>
-                    </div>    
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Notifications
-                        </span>
-                    </div>    
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Privacy and security
-                        </span>
-                    </div>  
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Supervision
-                        </span>
-                    </div>  
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Emails from Instagram
-                        </span>
-                    </div>
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Report a problem
-                        </span>
-                    </div>
-
-                    <div class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
-                        <span class="font-sans text-sm font-thin text-white">
-                            Log Out
-                        </span>
-                    </div> 
-
+        <div class="space-y-6 rounded-full">
+                <div class="flex flex-col m-1 max-h-full overflow-y-scroll scrollbar">
                     <div 
-                        @click="onModalClosed()"
-                        class="flex w-full justify-center p-2 hover:cursor-pointer">
+                        v-for="(setting, index) in settings"
+                        :key="index"
+                        @click="setting.action()"
+                        class="flex w-full border-b border-gray-700 justify-center p-2 hover:cursor-pointer">
+
                         <span class="font-sans text-sm font-thin text-white">
-                            Cancel
+                            {{ setting.name }}
                         </span>
-                    </div> 
-                    
+
+                    </div>
                 </div>
         </div>
         
     </div>
-
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
 import SmallCard from '@/components/cards/SmallCard.vue';
 import SVGLoader from '@/components/basics/SVGLoader.vue';
@@ -88,6 +35,60 @@ import SVGLoader from '@/components/basics/SVGLoader.vue';
 export default defineComponent({
     name: "settingModal",
     setup(prop, context) {
+
+      const router = useRouter();
+
+      const settings = [
+        {
+            name: 'Change Password',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'QR Code',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'Apps and Websites',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'Notifications',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'Privacy and security',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'Supervision',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'Emails from Instagram',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'Report a problem',
+            action: () => goToSettingRoute()
+        },
+        {
+            name: 'Log Out',
+            action: () => logout()
+        },
+        {
+            name: 'Cancel',
+            action: () => onModalClosed()
+        },
+      ]
+
+      const goToSettingRoute = () => {
+          router.push('/settings');
+      }
+
+      const logout = () => {
+        // TODO: Add logout logic
+          router.push('/logout');
+      }
 
       /**
        * Emit signal when the modal is closed
@@ -99,7 +100,9 @@ export default defineComponent({
         onMounted(() => {
         });
         return {
-            onModalClosed
+            onModalClosed,
+            goToSettingRoute,
+            settings
         };
     },
     props: {
