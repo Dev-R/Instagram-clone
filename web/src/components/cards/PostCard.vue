f5653cdbdf337fd0c9bcb31fb37f85200eb5fd2f
Dev-R <Rabimhd@icloud.com>
2023-09-27 23:25:38 +0300

    Improve PostCard

--- web/src/components/cards/PostCard.vue
+++ web/src/components/cards/PostCard.vue
@@ -1,129 +1,146 @@
 <template>
-    <div class="flex flex-col space-y-4 pt-2">
-
+    <div class="flex flex-col space-y-2 sm:space-y-0">
         <!-- Header-->
         <div class="flex rounded-lg space-x--1 justify-between">
             <!-- 1: username | daysSinceUpload | options -->
             <div class="flex space-x-2">
 
-                <img 
-                    :src="postItem.profilePictureUrl" 
-                    class="cursor-pointer h-8 w-8 rounded-full 
-                    shadow-lg">
+                <div class="story-avatar">
+                        <a href="#" class="block bg-white rounded-full relative">
+                            <img 
+                                class="w-8 h-8 rounded-full object-cover p-0.5 bg-black"
+                                :src="postItem.profilePictureUrl"/>
+                        </a>
+                </div>
                     
                 <div class="flex pt-1">
-                    <div class="cursor-pointer font-sans text-sm font-semibold text-white">
+                    <div class="cursor-pointer font-sans text-sm font-semibold text-white self-center">
                         {{ postItem.userName }}
-                        <i class="fa-solid fa-circle-check"></i>
+                        <!-- <i class="fa-solid fa-circle-check"></i> -->
                     </div>
 
-                    <div class="text-gray-500 w-5 font-sans text-md font-semibold">
+                    <div class="text-gray-500 w-5 font-sans text-md font-semibold self-center px-2">
                         •
                     </div>
 
-                    <div class="font-sans text-sm font-semibold text-gray-500">
+                    <div class="font-sans text-sm font-light text-[#949494] self-center">
                         {{ postItem.createdAt }}
                     </div>
                 </div>
 
             </div>
 
-            <div class="cursor-pointer">
-                <SVGLoader :icon="'more-options-svg'" />
+            <div class="cursor-pointer self-end">
+                <SVGLoader 
+                    :icon="'more-options'" />
             </div>
 
         </div>
-        <!-- 2: Medias -->
-        <div class="md:max-h-[585px] p-1">
-            <!-- <img src="https://loremflickr.com/1024/1280" class="rounded"> -->
-            <MediaCarousel 
-                v-if="postItem.carouselMedia"
-                :medias="postItem.carouselMedia"/>
-        </div>
-        <!-- 3: Actions -->
-        <div class="flex justify-between">
 
-            <div class="flex space-x-4">
+        <div class="flex flex-col space-y-3">
+            <!-- 2: Medias -->
+            <div class="md:max-h-[585px] px-0.5">
+                <!-- <img src="https://loremflickr.com/1024/1280" class="rounded"> -->
+                <MediaCarousel
+                    v-if="postItem.carouselMedia"
+                    :medias="postItem.carouselMedia" />
+            </div>
 
-                <span 
-                    @click="$emit('onPostLike', postItem.id)"
-                    class="cursor-pointer hover:scale-90">
-                    <SVGLoader 
-                        v-if="postItem.hasLiked" :icon="'like'"/>
-                    
-                    <SVGLoader 
-                        v-else :icon="'unlike'"/>
-                </span>
+            <!-- 3: Actions -->
+            <div class="flex justify-between">
+                <div class="flex space-x-4">
+                    <span
+                        @click="$emit('onPostLike', postItem.id)"
+                        class="cursor-pointer hover:scale-90">
+                        <SVGLoader
+                            v-if="postItem.hasLiked" :icon="'like'" />
+            
+                        <SVGLoader
+                            v-else :icon="'unlike'" />
+                    </span>
+                    <span
+                        @click="onOpenCommentModal(postItem.id)"
+                        class="cursor-pointer hover:scale-90">
+                        <SVGLoader
+                            :icon="'comment'" />
+                    </span>
+                    <span class="cursor-pointer hover:scale-90">
+                        <SVGLoader
+                            :icon="'share'" />
+                    </span>
+                </div>
+                <div class="cursor-pointer hover:scale-90">
+                    <SVGLoader
+                        :icon="'save'" />
+                </div>
+            </div>
 
-                <span 
+            <!-- 4: Likes -->
+            <div class="cursor-pointer font-sans text-sm font-semibold text-white self-start">
+                {{ findNumberOfLikes }}
+            </div>
+
+            <!-- 5: Caption -->
+            <div class="font-sans text-sm text-white flex-col">
+                <!-- <span class="font-semibold">imamomarsuleiman</span> -->
+                <p class="text-sm text-left indent-8 break-all ">
+                    {{ postItem.caption }}
+                </p>
+                <p
+                    v-if="postItem.commentCount > 0"
                     @click="onOpenCommentModal(postItem.id)"
-                    class="cursor-pointer hover:scale-90">
-                    <SVGLoader :icon="'comment'" />
-                </span>
-
-                <span class="cursor-pointer hover:scale-90">
-                    <SVGLoader :icon="'share'" />
-                </span>
-
+                    class="text-md text-left
+                    text-gray-400 cursor-pointer">
+                    View all {{ postItem.commentCount }} comments
+                </p>
             </div>
-
-            <div class="cursor-pointer hover:scale-90">
-                <SVGLoader :icon="'save'" />
+            
+            <!-- 6: Comment Form -->
+            <div class="grid grid-cols-12 border-b border-slate-800 p-2">
+                <span class="col-span-10">
+                    <textarea
+                        rows="1"
+                        class="focus:outline-none resize-none border-none
+                        block w-full text-sm bg-black text-gray-600"
+                        placeholder="Add a comment..."></textarea>
+                </span>
+                <span
+                    class="font-sans text-xs text-sky-500
+                    justify-self-end cursor-pointer
+                    hover:text-white h-6">
+                    Post
+                </span>
+                <span
+                    class="text-gray-600 justify-self-end
+                    cursor-pointer fa-regular fa-face-smile
+                    w-3 h-3">
+                </span>
             </div>
-
         </div>
 
-        <!-- 4: Likes -->
-        <div class="cursor-pointer font-sans text-sm font-semibold text-white self-start">
-            {{ findNumberOfLikes }}
-        </div>
-
-        <!-- 5: Caption -->
-        <div class="font-sans text-sm text-white flex-col">
-            <!-- <span class="font-semibold">imamomarsuleiman</span> -->
-            <p class="text-sm text-left indent-8 break-all ">
-                {{ postItem.caption }}
-            </p>
-            <p
-                v-if="postItem.commentCount > 0" 
-                @click="onOpenCommentModal(postItem.id)"
-                class="text-md text-left 
-                text-gray-400 cursor-pointer">
-                View all {{ postItem.commentCount }} comments
-            </p>
-        </div>
-
-        <!-- 6: Comment Form -->
-        <div class="grid grid-cols-12 border-b border-slate-800 p-2">
-            <span class="col-span-10">
-                <textarea 
-                    rows="1"
-                    class="focus:outline-none resize-none 
-                    block w-full text-sm bg-black text-gray-600"
-                    placeholder="Add a comment..."></textarea>
-            </span>
-            <span 
-                class="font-sans text-xs text-sky-500 
-                justify-self-end cursor-pointer 
-                hover:text-white h-6">
-                Post
-            </span>
-            <span 
-                class="text-gray-600 justify-self-end 
-                cursor-pointer fa-regular fa-face-smile 
-                w-3 h-3">
-            </span>
-        </div>
 
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue'
+import { 
+    defineComponent, 
+    ref, 
+    computed 
+} from 'vue'
+
+import {
+    // SVGLoader,
+    // MediaCarousel
+} from '@/components'
 
 import SVGLoader from '@/components/basics/SVGLoader.vue'
 import MediaCarousel from '@/components/carousels/MediaCarousel.vue'
-import type { PostCard } from '@/common'
+
+import type { 
+    PostCard 
+} from '@/common'
+
 
 export default defineComponent({
     name: 'PostCard',
@@ -142,7 +159,7 @@ export default defineComponent({
          * @event comment-unliked
          * @param {string} postId - The ID of the post
          */
-        const onOpenCommentModal = (postId: string) => {
+        const onOpenCommentModal = (postId: string | undefined) => {
             isCommentModalOpen.value = true;
             console.log("Emitting signal:", postId)
             context.emit('onOpenCommentModal', postId);
@@ -170,3 +187,12 @@ export default defineComponent({
     ]
 })
 </script>
+
+<style scoped>
+.story-avatar {
+    position: relative;
+    border-radius: 50%;
+    padding: 2.0px;
+    background-image: linear-gradient(to right top,#ffc600 20%,#ff0040,#e600cc 80%);
+}
+</style>
