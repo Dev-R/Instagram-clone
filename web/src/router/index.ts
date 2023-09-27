09641323fbc9aaa9b03ec5e119412de535b549bc
Dev-R <Rabimhd@icloud.com>
2023-09-27 03:37:02 +0300

    Add search route to router

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -50,6 +50,12 @@ const router = createRouter({
           meta: { title: 'Reels' }
         },
         {
+          path: '/search',
+          name: 'search',
+          component: () => import('@/views/SearchView.vue'),
+          meta: { title: 'Search' }
+        },
+        {
           path: '/settings',
           name: 'setting',
           component: () => import('@/views/SettingView.vue'),
