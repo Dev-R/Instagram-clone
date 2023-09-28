61993a65183731ff902dbd78a6d05888f6cb192d
Dev-R <Rabimhd@icloud.com>
2023-09-28 03:42:00 +0300

    Refactor MessagesHeader

--- web/src/components/direct/MessagesHeader.vue
+++ web/src/components/direct/MessagesHeader.vue
@@ -1,30 +1,25 @@
 <template>
         <div class="flex flex-col">
-            <div 
-                class="flex bg-black p-5 lg:h-16 
-                w-full space-x-2 justify-between">
-                <div class="flex">
-                    <div class="cursor-pointer font-sans 
-                    text-xl font-bold text-white pt-1">
-                        {{ currentUser?.userName }}
-                    </div>
 
-                    <div class="flex space-x-4 cursor-pointer">
-                    </div>
-                </div>
+            <div class="flex bg-black px-2 py-5 lg:h-16 w-full space-x-2 justify-between">
+                <SVGLoader 
+                    @click="onPageBack"
+                    :class="'rotate-[270deg] cursor-pointer sm:hidden'"
+                    :icon="'back-arrow'" />
 
-                <div>
+                <div class="cursor-pointer font-sans text-xl font-bold text-white pt-1">
+                    {{ currentUser?.userName }}
                 </div>
                     
                 <div 
                     class="flex cursor-pointer"
                     @click="emitNewMessage()">
-                    <SVGLoader :icon="'new-message'" />
+                    <SVGLoader 
+                        :icon="'new-message'" />
                 </div>
             </div>
 
-            <div 
-                class="flex bg-black pl-5 pr-5 pb-2 w-full space-x-2 justify-between">
+            <div class="flex bg-black pl-5 pr-5 pb-2 w-full space-x-2 justify-between">
                 <div class="flex">
                     <div class="cursor-pointer font-sans text-md font-bold text-white pt-1">
                         Messages
@@ -34,36 +29,55 @@
                     </div>
                 </div>
 
-                <div>
-                </div>
+                <div></div>
                     
                 <div class="cursor-pointer font-sans text-xs font-semibold text-[#949494] pt-1">
                     Requests
                 </div>
             </div>
+
         </div>
-    
 </template>
 
 <script lang="ts">
-import type { Viewer } from '@/common/models'
-import { defineComponent, onMounted } from 'vue'
-import SVGLoader from '../basics/SVGLoader.vue'
+import { 
+    defineComponent,
+    onMounted 
+} from 'vue'
 
+import { 
+    useRouter
+} from 'vue-router'
+
+import {
+    SVGLoader
+} from '@/components'
+
+import type { 
+    Viewer 
+} from '@/common'
 
 export default defineComponent({
     name: "MessagesHeader",
     setup(prop, context) {
+
+        // Services
+        const router = useRouter(); 
+
         /**
          * Emit new message
          */
         const emitNewMessage = () => {
             context.emit("onNewMessage");
         };
-        onMounted(() => {
-        });
+        
+        const onPageBack = () => {
+            router.back();
+        };
+        
         return {
-            emitNewMessage
+            emitNewMessage,
+            onPageBack
         };
     },
     props: {
