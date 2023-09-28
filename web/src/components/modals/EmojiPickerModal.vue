d1153981124b44e26720e96bb4ba38befd375366
Dev-R <Rabimhd@icloud.com>
2023-09-28 16:43:11 +0300

    Add EmojiPickerModal comonenet

---
+++ web/src/components/modals/EmojiPickerModal.vue
@@ -0,0 +1,45 @@
+<template>
+    <SVGLoader 
+        @click="openEmojiPicker"
+        :icon="'emoji'" />
+        
+    <div class="absolute bottom-12">
+        <Transition>
+            <EmojiPicker 
+                v-show="isToggled"
+                :native="false"
+                :theme="'dark'"
+                @select="emitSelectEmoji" />
+        </Transition>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+import { 
+    ref 
+} from 'vue'
+
+import EmojiPicker from 'vue3-emoji-picker'
+
+import {
+    SVGLoader
+} from '@/components'
+
+const emit = defineEmits(
+    ['selectEmoji']
+)
+
+// Data
+const isToggled = ref(false)
+
+// Methods
+const openEmojiPicker = () => {
+    isToggled.value = !isToggled.value
+}
+
+const emitSelectEmoji = (emoji: object) => {
+    emit('selectEmoji', emoji)
+}
+
+</script>
