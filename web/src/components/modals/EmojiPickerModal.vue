31ed55998ed780836b4eef737608231c902eb4a8
Dev-R <Rabimhd@icloud.com>
2023-09-28 20:13:26 +0300

    Add toggleDirection prop

--- web/src/components/modals/EmojiPickerModal.vue
+++ web/src/components/modals/EmojiPickerModal.vue
@@ -3,7 +3,9 @@
         @click="openEmojiPicker"
         :icon="'emoji'" />
         
-    <div class="absolute bottom-12 right-0 z-50">
+    <div 
+        class="absolute bottom-12 right-0 z-50"
+        :class="toggleDirectionClass()">
         <Transition>
             <EmojiPicker 
                 v-show="isToggled"
@@ -30,6 +32,12 @@ import type  {
     Emoji
 } from '@/common'
 
+const prop = defineProps({
+    toggleDirection: {
+        type: String as () => 'left' | 'right',
+        default: 'right'
+    }
+})
 const emit = defineEmits(
     ['selectEmoji']
 )
@@ -46,4 +54,7 @@ const emitSelectEmoji = (emoji: Emoji) => {
     emit('selectEmoji', emoji)
 }
 
+const toggleDirectionClass = () => {
+    return prop.toggleDirection === 'left' ? 'left-0' : 'right-0'
+}
 </script>
