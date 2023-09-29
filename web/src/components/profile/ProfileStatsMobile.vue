0c293dcbe70ee4c523f60f92d30320c60d720d47
Dev-R <Rabimhd@icloud.com>
2023-09-29 03:44:37 +0300

    Add ProfileStatsMobile componenet

---
+++ web/src/components/profile/ProfileStatsMobile.vue
@@ -0,0 +1,72 @@
+<template>
+    <div 
+        class="text-center border-t 
+        md:hidden block border-slate-1100 mt-2">
+        <ul 
+            class="flex space-x-14 flex-wrap 
+            justify-around pt-3 pl-4 pr-4">
+            <div 
+                v-for="(element, index) of userProfileStats"
+                :key="index"
+                class="flex flex-col sm:hover:cursor-pointer"
+                @click="element.action">
+                <span class="text-sm subpixel-antialiase text-white">
+                    {{ element.count }}
+                </span>
+
+                <span class="text-sm subpixel-antialiase text-gray-400">
+                    {{ element.title }}
+                </span>
+            </div>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type {
+    PropType
+} from 'vue'
+
+import {
+    ModalType,
+    type User,
+} from '@/common'
+
+const prop = defineProps({
+    user: {
+        type: Object as PropType<User>,
+        required: true
+    }
+})
+const emit = defineEmits(['openModal'])
+
+// Data
+const userProfileStats = [{
+        title: 'posts',
+        count: prop.user.mediaCount, // TODO: Change mediaCount to postCount
+        action: () => {},
+    },
+    {
+        title: 'followers',
+        count: prop.user.followerCount,
+        action: () => emitModal(ModalType.Follow, 'Followers'),
+    },
+    {
+        title: 'following',
+        count: prop.user.followingCount,
+        action: () => emitModal(ModalType.Follow, 'Following'),
+    }
+]
+
+/**
+ *  Emits a modal event to open a modal
+ * @param modalType The type of modal to open (Follow, Settings, etc.)
+ * @param modalTitle The title of the modal to open (Followers, Following, etc.)
+ */
+const emitModal = (modalType: ModalType, modalTitle: string) => {
+    emit('openModal', {
+        modalType,
+        modalTitle
+    })
+}
+</script>
