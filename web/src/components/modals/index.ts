89b0d008f1d266b7d5934d5e74aadfefb9da4346
Dev-R <Rabimhd@icloud.com>
2023-09-29 03:47:24 +0300

    Update modals imports

--- web/src/components/modals/index.ts
+++ web/src/components/modals/index.ts
@@ -1,13 +1,13 @@
 import PhotoModal from "./PhotoModal.vue"
 import CommentModal from "./CommentModal.vue"
-import SettingModal from "./SettingModal.vue"
+import ProfileSettingModal from "./ProfileSettingModal.vue"
 import SmallModal from "./SmallModal.vue"
 import EmojiPickerModal from "./EmojiPickerModal.vue"
 
 export {
     PhotoModal,
     CommentModal,
-    SettingModal,
+    ProfileSettingModal,
     SmallModal,
     EmojiPickerModal
 }
