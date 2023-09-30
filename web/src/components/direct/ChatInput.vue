8f5e899b964b98e20b9511334de32c5c918a86d2
Dev-R <Rabimhd@icloud.com>
2023-09-30 03:52:44 +0300

    Refactor ChatInput

--- web/src/components/direct/ChatInput.vue
+++ web/src/components/direct/ChatInput.vue
@@ -1,128 +1,112 @@
 <template>
-    <div class="flex absolute inset-x-3 md:bottom-2 bottom-8 rounded-full space-x-3">
-            <!-- Emojis -->
-            <div class="cursor-pointer inset-y-0 left-0 \
+	<div class="flex absolute inset-x-3 md:bottom-2 bottom-8 rounded-full space-x-3">
+		<!-- Emojis -->
+		<div
+			class="cursor-pointer inset-y-0 left-0 \
                     flex items-center">
-                <span class="relative">
-                    <EmojiPickerModal
-                        :toggleDirection="'left'" 
-                        @selectEmoji="appendEmoji" />
-                </span>
-            </div>                             
+			<span class="relative">
+				<EmojiPickerModal
+					:toggle-direction="'left'" 
+					@selectEmoji="appendEmoji" />
+			</span>
+		</div>                             
                 
-            <!-- Chat Area -->
-            <textarea
-                ref="textArea"
-                tabindex="1"
-                rows="1"
-                maxlength="100"
-                type="text"
-                class="bg-black border border-[#262626] text-white 
+		<!-- Chat Area -->
+		<textarea
+			ref="textArea"
+			tabindex="1"
+			rows="1"
+			maxlength="100"
+			type="text"
+			class="bg-black border border-[#262626] text-white 
                 rounded-full text-sm focus:outline-none
                 block w-full p-2.5 resize-none"
-                placeholder="Message..."
-                :class="{ 'rounded-lg ': !isChatEmpty }"
-                :value="modalValue" 
-                @keyup.enter="emitSendMessage" />
+			placeholder="Message..."
+			:class="{ 'rounded-lg ': !isChatEmpty }"
+			:value="modalValue" 
+			@keyup.enter="emitSendMessage"></textarea>
             
-            <!-- Gallery -->
-            <div
-                class="self-center"
-                :class="{ 'hidden': !isChatEmpty }"
-                @click="emitFileUpload">
-                <SVGLoader
-                    :icon="'gallery'" 
-                    :class="'cursor-pointer md:absolute inset-y-0 right-20 \
+		<!-- Gallery -->
+		<div
+			class="self-center"
+			:class="{ 'hidden': !isChatEmpty }"
+			@click="emitFileUpload">
+			<SVGLoader
+				:icon="'gallery'" 
+				:class="'cursor-pointer md:absolute inset-y-0 right-20 \
                     flex items-center'" /> 
-            </div>
+		</div>
 
-            <!-- Like/Heart -->
-            <div
-                class="self-center"
-                :class="{ 'hidden': !isChatEmpty }"
-                @click="emitLikeIcon()">
-                <SVGLoader
-                    :icon="'like'" 
-                    :class="'cursor-pointer md:absolute inset-y-0 right-12 \
+		<!-- Like/Heart -->
+		<div
+			class="self-center"
+			:class="{ 'hidden': !isChatEmpty }"
+			@click="emitLikeIcon()">
+			<SVGLoader
+				:icon="'like'" 
+				:class="'cursor-pointer md:absolute inset-y-0 right-12 \
                     flex items-center'" />     
-            </div>
-    </div>
+		</div>
+	</div>
 </template>
 
-<script lang="ts">
-import { 
-    defineComponent, 
-    onMounted, 
-    ref 
-} from 'vue';
+<script setup lang="ts">
+import {
+    ref
+} from 'vue'
 
 import {
     EmojiPickerModal,
     SVGLoader
-} from '@/components';
+} from '@/components'
 
 import type {
     Emoji
-} from '@/common';
-export default defineComponent({
-    name: "ChatInput",
-    setup(prop, context) {
-        // DOM Refs
-        const textArea = ref<HTMLInputElement | null>()
-        /**
-         * Emit new message
-         */
-        const emitSendMessage = (payload: KeyboardEvent) => {
-            context.emit("onSendMessage", payload);
-        };
-        /**
-         * Emit like icon
-         */
-        const emitLikeIcon = () => {
-            context.emit("onLikeIcon");
-        };
-        /**
-         * Emit file upload
-         */
-        const emitFileUpload = () => {
-            context.emit("onFileUpload");
-        };
-        /**
-         * Append emoji to textarea
-         */
-        const appendEmoji = (emoji: Emoji) => {
-            if (textArea.value) {
-                textArea.value.focus();
-                textArea.value.value += emoji.i;
-            }
-        };
-        onMounted(() => {
-        });
+} from '@/common'
 
-        return {
-            emitLikeIcon,
-            emitSendMessage,
-            emitFileUpload,
-            appendEmoji,
-            textArea
-        };
+defineProps({
+    modalValue: {
+        type: String as () => string | number | string[] | undefined,
+        default: undefined
     },
-    props: {
-        modalValue: {
-            type: String as () => string | number | string[] | undefined,
-            default: undefined
-        },
-        isChatEmpty: {
-            type: Boolean,
-            required: true
-        }
-    },
-    emits: [
-        "update:modelValue",
-        "onLikeIcon",
-        "onSendMessage",
-        "onFileUpload"
-    ],
-    components: { SVGLoader, EmojiPickerModal }
-})
+    isChatEmpty: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const emit = defineEmits([
+    "onLikeIcon",
+    "onSendMessage",
+    "onFileUpload"
+]);
+// DOM Refs
+const textArea = ref<HTMLInputElement | null>()
+/**
+ * Emit new message
+ */
+const emitSendMessage = (payload: KeyboardEvent) => {
+    emit("onSendMessage", payload);
+};
+/**
+ * Emit like icon
+ */
+const emitLikeIcon = () => {
+    emit("onLikeIcon");
+};
+/**
+ * Emit file upload
+ */
+const emitFileUpload = () => {
+    emit("onFileUpload");
+};
+/**
+ * Append emoji to textarea
+ */
+const appendEmoji = (emoji: Emoji) => {
+    if (textArea.value) {
+        textArea.value.focus();
+        textArea.value.value += emoji.i;
+    }
+};
 </script>
