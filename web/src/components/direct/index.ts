17f3d6ce024f92fbbdb35af89e7611b0d9eed3a1
Dev-R <Rabimhd@icloud.com>
2023-09-30 04:00:22 +0300

    Add missing exports

--- web/src/components/direct/index.ts
+++ web/src/components/direct/index.ts
@@ -1,9 +1,19 @@
 import ChatIntro from './ChatIntro.vue'
+import ChatHeader from './ChatHeader.vue'
+import ChatMessage from './ChatMessage.vue'
+import ChatInput from './ChatInput.vue'
+import MessagesHeader from './MessagesHeader.vue'
+import MessagesList from './MessagesList.vue'
 import TheChat from './TheChat.vue'
 import TheMessages from './TheMessages.vue'
 
 export {
-  ChatIntro,
   TheChat,
-  TheMessages
+  TheMessages,
+  MessagesHeader,
+  MessagesList,
+  ChatIntro,
+  ChatHeader,
+  ChatMessage,
+  ChatInput
 }
