73b19bec3e62bc8962fdbb2b858a10003f86249f
Dev-R <Rabimhd@icloud.com>
2023-09-30 04:14:58 +0300

    Reformat MobileSearchBar

--- web/src/components/search/MobileSearchBar.vue
+++ web/src/components/search/MobileSearchBar.vue
@@ -1,23 +1,27 @@
 <template>
-    <div class="flex justify-between items-center space-x-2 p-2.5 pb-2 border-b border-gray-600 sm:hidden">
-        <input
-        @focus="navigateToSearch"
-        class="w-full bg-black text-white border-1 border-gray-400 rounded-lg h-8
-        text-sm focus:outline-none placeholder-gray-400"
-        placeholder="Search"/>
+	<div class="flex justify-between items-center space-x-2 p-2.5 pb-2 border-b border-gray-600 sm:hidden">
+		<input
+			class="w-full bg-black text-white border-1 border-gray-400 rounded-lg h-8
+            text-sm focus:outline-none placeholder-gray-400"
+			placeholder="Search"
+			@focus="navigateToSearch" />
 
-        <div class="text-sm text-white font-sans font-semibold">
-            Cancel
-        </div>
-    </div>
+		<div class="text-sm text-white font-sans font-semibold">
+			Cancel
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
+import {
+    useRouter
+} from 'vue-router'
+
 const emit = defineEmits([
     'on-search-query'
 ])
 
 const navigateToSearch = () => {
-    emit('on-search-query')
+    useRouter().push('/search')
 }
 </script>
