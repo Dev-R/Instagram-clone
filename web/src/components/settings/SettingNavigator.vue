b50676e86c503c165727bab570b46405327897e8
Dev-R <Rabimhd@icloud.com>
2023-09-30 21:20:14 +0300

    Add SettingNavigator

---
+++ web/src/components/settings/SettingNavigator.vue
@@ -0,0 +1,31 @@
+<template>
+	<div 
+		:class="{
+			'hidden': isMobileScreen && activeSettingTab !== null,
+		}"
+		class="flex flex-col h-full basis-2/6 font-sans font-normal text-xs text-white border-r border-[#363636]">
+		<SettingInfo />
+
+		<SettingTabs 
+			@edit-profile="$emit('edit-profile')" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import {
+    SettingInfo,
+    SettingTabs
+} from '@/components'
+
+defineProps({
+    isMobileScreen: {
+        type: Boolean,
+        required: true
+    },
+    activeSettingTab: {
+        type: String as () => string | null
+    }
+})
+
+defineEmits(['edit-profile'])
+</script>
