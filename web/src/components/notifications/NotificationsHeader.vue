536bdb94ab3865500d31e1cebbf7f6f8d2f1df6e
Dev-R <Rabimhd@icloud.com>
2023-09-30 05:31:48 +0300

    Add NotificationsHeader

---
+++ web/src/components/notifications/NotificationsHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="text-xl font-sans sm:text-2xl text-white font-bold hidden sm:block">
+        Notifications
+    </div>
+
+    <div 
+        class="text-md font-sans sm:text-xl text-white font-bold sm:pt-4 
+        sm:border-t-2 border-gray-900 text-center sm:text-left">
+        Latest
+    </div>
+</template>
