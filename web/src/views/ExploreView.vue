148345a0d19512f67111a4ae48a193386eb5aa32
Dev-R <Rabimhd@icloud.com>
2023-09-30 04:12:55 +0300

    Migrate expore view to setup script

--- web/src/views/ExploreView.vue
+++ web/src/views/ExploreView.vue
@@ -1,10 +1,8 @@
 <template>
 	<div class="bg-black h-screen">
-		<section 
-			v-show="!activeModal.isToggled"
+		<section
 			class="container md:max-w-full mx-auto h-screen
-			scrollbar scrollbar-thumb-gray-900"
-			:class="{ 'brightness-50 pointer-events-none': isModalToggled }">
+			scrollbar scrollbar-thumb-gray-900">
 			<div class="md:grid grid-cols-12 flex justify-center">
 				<!-- Left bar: Navigation -->
 				<div 
@@ -32,106 +30,67 @@
 						<div
 							class="flex flex-wrap">
 							<PostCoverCard 
-								@on-post-cover="triggerCommentModal"
 								:posts="posts" />
 						</div>
 					</div>
 				</div>
 			</div>
 		</section>
-		<!-- Modals -->
-
-		<!-- PostCard Modal -->
-		<div 
-			v-show="activeModal.name === ModalName.PROFILE"
-			class="md:w-[470px] justify-self-end p-2">
-			<PostCardModal
-				:post-item="posts[activeModal.postId]" />
-		</div>
-
-		<!-- Comment Modal -->
-		<CommentModal
-			:post-comment="{
-				isToggled: activeModal.isToggled && activeModal.name === ModalName.COMMENT,
-				post : posts[activeModal.postId],
-			}" 
-			@on-comment-liked="handleCommentLike"
-			@on-add-comment="handleCommentAdd"
-			@on-post-liked="handlePostLike"
-			@on-modal-closed="triggerCommentModal" />
 	</div>
 </template>
  
-<script lang="ts">
+<script setup lang="ts">
 import {
-	onMounted,
-	defineComponent,
-	ref,
-	computed,
-onUnmounted
+    ref
 } from 'vue'
 
-import { 
-  useRouter 
+import {
+    useRouter
 } from 'vue-router'
 
 import {
-	NavBarMain,
-	CommentModal,
-	SVGLoader,
-	PostCard as PostCardModal,
-  PostCoverCard,
-  MobileSearchBar
+    NavBarMain,
+    PostCoverCard,
+    MobileSearchBar
 } from '@/components'
 
 
-import {
-	type PostCard as PostCardType,
-	type PostMedia,
-	ModalName
+import type {
+    PostCard as PostCardType,
+    PostMedia,
 } from '@/common'
 
 // Sample Data
-const mediasArraySampleA: PostMedia[] = [
-	{
-		index: 0,
-		type: 'image',
-		mediaUrl:
-            'https://loremflickr.com/1024/1280/cat',
-		title: 'Legendary A'
-	},
-	{
-		index: 1,
-		type: 'image',
-		mediaUrl:
-            'https://loremflickr.com/1024/1280/nature',
-		title: 'Legendary A'
-	}
+const mediasArraySampleA: PostMedia[] = [{
+        index: 0,
+        type: 'image',
+        mediaUrl: 'https://loremflickr.com/1024/1280/cat',
+        title: 'Legendary A'
+    },
+    {
+        index: 1,
+        type: 'image',
+        mediaUrl: 'https://loremflickr.com/1024/1280/nature',
+        title: 'Legendary A'
+    }
 ]
 
-const mediasArraySampleB: PostMedia[] = [
-	{
-		index: 0,
-		type: 'image',
-		mediaUrl:
-            'https://loremflickr.com/1024/1280/life',
-		title: 'Legendary A'
-	},
-	{
-		index: 1,
-		type: 'image',
-		mediaUrl:
-            'https://loremflickr.com/1024/1280/car',
-		title: 'Legendary A'
-	}
+const mediasArraySampleB: PostMedia[] = [{
+        index: 0,
+        type: 'image',
+        mediaUrl: 'https://loremflickr.com/1024/1280/life',
+        title: 'Legendary A'
+    },
+    {
+        index: 1,
+        type: 'image',
+        mediaUrl: 'https://loremflickr.com/1024/1280/car',
+        title: 'Legendary A'
+    }
 ]
 
-export default defineComponent({
-  name: 'Reels',
-  setup() {
-    // Sample Data
-    const posts = ref<PostCardType[]>([
-      {
+// Sample Data
+const posts = ref < PostCardType[] > ([{
         id: '0',
         userName: 'Rabee',
         createdAt: 'February 24',
@@ -143,8 +102,8 @@ export default defineComponent({
         profilePictureUrl: 'https://loremflickr.com/32/32/life',
         isFollowed: false,
         comments: []
-      },
-      {
+    },
+    {
         id: '1',
         userName: 'Rabee',
         createdAt: 'February 24',
@@ -156,131 +115,13 @@ export default defineComponent({
         profilePictureUrl: 'https://loremflickr.com/32/32/dog',
         isFollowed: false,
         comments: []
-      }
-    ])
-
-    // Services
-    const router = useRouter()
-
-    // Modals data
-    const activeModal = ref({
-      name: '' as ModalName,
-      isToggled: false,
-      postId: 0
-    })
-
-    /**
-     * Triggers the comment modal.
-     * If screen size > 768, opens comment modal, else opens profile modal.
-     * @param postId The ID of the post.
-     */
-    const triggerCommentModal = (postId: number | undefined) => {
-      const modalName = screenSizeType.value === 'xs' ? ModalName.PROFILE : ModalName.COMMENT
-      activeModal.value = { name: modalName, isToggled: !activeModal.value.isToggled, postId: postId ? postId : 0 }
-
-      console.log("Triggering the following modal:", modalName)
     }
+])
 
-    /**
-     * Handles post like.
-     * TODO: Remove.
-     */
-    const handlePostLike = () => {
-      posts.value[activePostId.value].hasLiked = !posts.value[activePostId.value].hasLiked
-      console.log("Post has been liked ...")
-    }
-
-    /**
-     * Navigates to search page.
-     */
-    const navigateToSearch = () => {
-      console.log("Navigating to search page ...")
-      router.push('/search')
-    }
-
-    /**
-     * Handles comment addition.
-     * TODO: Remove.
-     * @param commentContent The content of the comment.
-     */
-    const handleCommentAdd = (commentContent: string) => {
-      posts.value[activePostId.value].comments?.push({
-        id: 0,
-        userName: 'Sara',
-        profilePictureUrl: 'https://loremflickr.com/1024/1280/woman',
-        content: commentContent,
-        createdAt: '2012-02-23'
-      })
-      console.log("New comment has been added ...", posts.value[activePostId.value].comments)
-    }
-
-    /**
-     * Handles comment like.
-     * @param commentId The ID of the comment.
-     */
-    const handleCommentLike = (commentId: number) => {
-      console.log("The following comment", commentId, "has been liked")
-    }
-
-    /**
-     * Adds the resize event listener.
-     */
-    const addResizeListener = () => {
-      window.addEventListener('resize', onWidthChange)
-    }
-
-    /**
-     * Removes the resize event listener.
-     */
-    const removeResizeListener = () => {
-      window.removeEventListener('resize', onWidthChange)
-    }
-
-    /**
-     * Updates the window width on resize.
-     */
-    const onWidthChange = () => {
-      windowWidth.value = window.innerWidth
-    }
-
-    // Computed
-    const windowWidth = ref(window.innerWidth) // Current window width
-    const screenSizeType = computed(() => (windowWidth.value < 550 ? 'xs' : false))
-    const isModalToggled = computed(() => activeModal.value.isToggled)
-    const activePostId = computed(() => activeModal.value.postId)
-
-    // Lifecycle Hooks
-    onMounted(() => {
-      addResizeListener()
-    })
-
-    onUnmounted(() => {
-      removeResizeListener()
-    })
-
-    return {
-      triggerCommentModal,
-      handleCommentAdd,
-      handleCommentLike,
-      handlePostLike,
-      posts,
-      ModalName,
-      activeModal,
-      navigateToSearch,
-      isModalToggled
-    }
-  },
-  components: {
-    NavBarMain,
-    SVGLoader,
-    CommentModal,
-    PostCardModal,
-    PostCoverCard,
-    MobileSearchBar
-},
-})
-
+/**
+ * Navigates to search page.
+ */
+const navigateToSearch = () => {
+    useRouter().push('/search')
+}
 </script>
-
-<style scoped>
-</style>
