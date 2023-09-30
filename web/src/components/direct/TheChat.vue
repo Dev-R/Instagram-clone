1c694b20bd2917cd6ad1b921c1160681f4334e73
Dev-R <Rabimhd@icloud.com>
2023-09-30 03:56:35 +0300

    Refactor TheChat

--- web/src/components/direct/TheChat.vue
+++ web/src/components/direct/TheChat.vue
@@ -1,85 +1,75 @@
 <template>
-    <div 
-        class="relative bg-black lg:basis-10/12 
+	<div 
+		class="relative bg-black lg:basis-10/12 
         w-full h-screen border-l border-gray-800">
-
-        <!-- Chat Header -->
-        <ChatHeader
-            @on-chat-back="$emit('onChatBack')"
-            :current-user-name="currentUser.firstName"/>
+		<ChatHeader
+			:current-user-name="currentUser.firstName"
+			@on-chat-back="$emit('onChatBack')" />
             
-        <!-- Chat Body -->
-        <div class="flex flex-col lg:max-h-[850px] overflow-auto">
-            <!-- C -->
-            <div class="flex pt-5 space-x-2 justify-center">
-                <span class="font-sans text-xs font-semibold text-gray-400">
-                    November 2, 2020 12:44 am
-                </span>
-                <i  
-                    v-if="isChatLoading"
-                    class="fa-solid fa-spinner fa-spin fa-2xl text-slate-500">
-                </i>
-            </div>
+		<div class="flex flex-col lg:max-h-[850px] overflow-auto">
+			<div class="flex pt-5 space-x-2 justify-center">
+				<span class="font-sans text-xs font-semibold text-gray-400">
+					November 2, 2020 12:44 am
+				</span>
+				<i  
+					v-if="isChatLoading"
+					class="fa-solid fa-spinner fa-spin fa-2xl text-slate-500">
+				</i>
+			</div>
 
-            <!-- Chat Messages -->
-            <ChatMessage
-                :active-conversation="activeConversation"/>
-        </div>
+			<ChatMessage
+				:active-conversation="activeConversation" />
+		</div>
 
-
-        <!-- Chat Input -->
-        <ChatInput 
-            @on-file-upload="$emit('onFileUpload')"
-            @on-like-icon="$emit('onLikeIcon')"
-            @on-send-message="$emit('onSendMessage', $event)"
-            :is-chat-empty="isChatEmpty" 
-            :value="modalValue"/>
-    </div>
+		<ChatInput 
+			:is-chat-empty="isChatEmpty"
+			:value="modalValue"
+			@on-file-upload="$emit('onFileUpload')"
+			@on-like-icon="$emit('onLikeIcon')" 
+			@on-send-message="$emit('onSendMessage', $event)" />
+	</div>
 </template>
 
-<script lang="ts">
-import type { Conversation, Viewer } from '@/common/models'
-import { defineComponent, onMounted } from 'vue'
-import ChatInput from './ChatInput.vue'
-import ChatMessage from './ChatMessage.vue'
-import ChatHeader from './ChatHeader.vue'
+<script setup lang="ts">
+import {
+    ChatInput,
+    ChatMessage,
+    ChatHeader
+} from '@/components'
+
+import type {
+    Conversation,
+    Viewer
+} from '@/common'
 
 
-export default defineComponent({
-    name: "TheChat",
-    setup(prop) {
-        onMounted(() => {
-        });
-        return {};
+defineProps({
+    activeConversation: {
+        type: Object as () => Conversation,
+        required: true
     },
-    props: {
-        activeConversation: {
-            type: Object as () => Conversation,
-            required: true
-        },
-        currentUser: {
-            type: Object as () => Viewer,
-            required: true
-        },
-        isChatLoading: {
-            type: Boolean,
-            required: true
-        },
-        isChatEmpty: {
-            type: Boolean,
-            required: true
-        },
-        modalValue: {
-            type: String as () => string | number | string[] | undefined,
-            default: undefined
-        }
+    currentUser: {
+        type: Object as () => Viewer,
+        required: true
     },
-    emits: [
-        "onLikeIcon",
-        "onSendMessage",
-        "onFileUpload",
-        "onChatBack",
-    ],
-    components: { ChatInput, ChatMessage, ChatHeader }
-})
+    isChatLoading: {
+        type: Boolean,
+        required: true
+    },
+    isChatEmpty: {
+        type: Boolean,
+        required: true
+    },
+    modalValue: {
+        type: String as () => string | number | string[] | undefined,
+        default: undefined
+    }
+});
+
+defineEmits([
+    "onLikeIcon",
+    "onSendMessage",
+    "onFileUpload",
+    "onChatBack",
+]);
 </script>
