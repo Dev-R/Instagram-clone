9409cf42fa39218cb2b58a53041f272808e53348
Dev-R <Rabimhd@icloud.com>
2023-09-30 05:31:33 +0300

    Add NotificationPanel

---
+++ web/src/components/notifications/NotificationPanel.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="bg-black scrollbar scrollbar-thumb-gray-900 md:p-0 p-2 w-full max-w-4xl mx-auto">
+		<div 
+			class="flex flex-col sm:border-r-2 border-gray-900 rounded-xl
+            flex-nowrap sm:space-y-4 pt-2 md:pt-5 justify-self-end h-full
+            md:ml-5 lg:ml-0">
+			<NotificationsHeader />
+
+			<NotificationFeed 
+				:is-loading="isNotificationsLoading"
+				:is-empty="isNotificationsEmpty"
+				:notifications="notifications" />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import type {
+    PropType,
+} from 'vue'
+
+import type {
+    NotificationCard as NotificationResult,
+} from '@/common'
+
+defineProps({
+    notifications: {
+        type: Array as PropType <NotificationResult[]> ,
+        required: true
+    },
+    isNotificationsLoading: Boolean,
+    isNotificationsEmpty: Boolean
+})
+
+</script>
